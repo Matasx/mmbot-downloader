4b0f46a26c92787eed68f1f22a8ddfa96610f222
Matasx <matasx@seznam.cz>
2021-12-16 13:46:25 +0100

    Refactoring.

--- Downloader.Core/Core/DownloadTaskExtensions.cs
+++ Downloader.Core/Core/DownloadTaskExtensions.cs
@@ -3,9 +3,9 @@ using Downloader.Core.Utils;
 
 namespace Downloader.Core.Core
 {
-    internal static class DownloadTaskExtensions
+    public static class DownloadTaskExtensions
     {
-        public static IEnumerable<SecChunk> ToSecChunks(this DownloadTask downloadTask, int dataCountLimit)
+        internal static IEnumerable<SecChunk> ToSecChunks(this DownloadTask downloadTask, int dataCountLimit)
         {
             var startSec = UnixEpoch.GetEpochSec(downloadTask.Start);
             var endSec = UnixEpoch.GetEpochSec(downloadTask.End);
@@ -17,7 +17,7 @@ namespace Downloader.Core.Core
                 .Select(i => new SecChunk(downloadTask.Symbol, startSec + i * increment, startSec + ((i + 1) * increment)));
         }
 
-        public static IEnumerable<MsChunk> ToMsChunks(this DownloadTask downloadTask, int dataCountLimit)
+        internal static IEnumerable<MsChunk> ToMsChunks(this DownloadTask downloadTask, int dataCountLimit)
         {
             var startSec = UnixEpoch.GetEpochMs(downloadTask.Start);
             var endSec = UnixEpoch.GetEpochMs(downloadTask.End);
@@ -32,11 +32,7 @@ namespace Downloader.Core.Core
         public static string ToFileName(this DownloadTask downloadTask)
         {
             var fileName = $"{downloadTask.Exchange}_{downloadTask.Symbol}_{downloadTask.Start.ToShortDateString()}_{downloadTask.End.ToShortDateString()}.csv";
-            foreach (var ch in Path.GetInvalidFileNameChars())
-            {
-                fileName = fileName.Replace(ch.ToString(), string.Empty);
-            }
-            return fileName;
+            return Path.GetInvalidFileNameChars().Aggregate(fileName, (current, ch) => current.Replace(ch.ToString(), string.Empty));
         }
     }
 }
